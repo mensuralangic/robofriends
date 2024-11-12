e3df2016f96412de0c1cd88ebae7a4513b16b069
mensuralangic <mensuralangic@gmail.com>
2024-11-12 15:45:48 +0100

    initial commit

---
+++ src/components/CardList.jsx
@@ -0,0 +1,18 @@
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+    return (  
+        <div>
+            {robots.map((robot) => (
+                <Card 
+                    key={robot.id} 
+                    id={robot.id} 
+                    name={robot.name} 
+                    email={robot.email}
+                />
+            ))} 
+        </div>
+    ); 
+}
+
+export default CardList; 
---
+++ src/containers/App.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react' 
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
+import './App.css'
+import Scroll from '../components/Scroll'
+
+class App extends Component  {
+    constructor()  {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ""
+        }
+
+    } 
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
+    }
+        
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})    
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+
+        return !this.state.robots.length ? <h1>Loading</h1> : (
+            <div className='tc'>
+                <h1 className='f2 mw-100'>Robofriends</h1>
+                <SearchBox searchChange = {this.onSearchChange}/>
+                {filteredRobots.length > 0 && <Scroll>
+                    <CardList robots={filteredRobots} />
+                </Scroll>}
+            </div>
+        );
+    }
+}
+    
+export default App; 
